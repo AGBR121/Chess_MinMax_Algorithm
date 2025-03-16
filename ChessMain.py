45a2bf3ce324d3879ea4e538667c7bcb96ed25ac
AGBR121 <angel.burbano@utp.edu.co>
2025-03-16 16:33:59 -0500

    add rules of moves without implementate check, checkmate and stalemate

--- ChessMain.py
+++ ChessMain.py
@@ -20,6 +20,8 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False
     loadImages()
     running = True
     sqSelected = ()
@@ -41,10 +43,21 @@ def main():
                 if len(playerClicks) == 2:
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
-                    sqSelected = ()
-                    playerClicks = []
-                    
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        playerClicks = [sqSelected]
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False         
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
